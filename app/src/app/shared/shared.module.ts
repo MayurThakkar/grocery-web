09222d553f0071ec2426386ee8c221416ec63e36
Mayur Thakkar <mayur.thakkar21@gmail.com>
2021-04-19 17:52:07 +0200

    Created separate module-> core modile, shared module and feature modile

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -3,15 +3,45 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../material.module';
-import { TranslateModule } from '@ngx-translate/core';
+import { MissingTranslationHandler, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { MainComponent } from '../main/feature/main.component';
+import { DialogComponent } from '../dialog/feature/dialog.component';
+import { MeterService } from '../meter/meter.service';
+import { AuthService } from '../auth/auth.service';
+import { UIService } from './ui.service';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+export class MyMissingTranslationHandler implements MissingTranslationHandler {
+  handle() {
+    return 'some value';
+  }
+}
 
 @NgModule({
+  declarations: [MainComponent, DialogComponent],
   imports: [
     CommonModule,
     FormsModule,
     MaterialModule,
     FlexLayoutModule,
-    TranslateModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'en',
+      missingTranslationHandler: {
+        provide: MissingTranslationHandler,
+        useClass: MyMissingTranslationHandler,
+      },
+      useDefaultLang: true,
+    }),
   ],
   exports: [
     CommonModule,
@@ -20,5 +50,6 @@ import { TranslateModule } from '@ngx-translate/core';
     FlexLayoutModule,
     TranslateModule,
   ],
+  providers: [MeterService, AuthService, UIService],
 })
 export class SharedModule {}
